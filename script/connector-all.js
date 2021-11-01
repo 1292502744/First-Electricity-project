561f9b575435e637e5b8c29b721a9d67bfcf45f3
Faith Yang <1292502744@qq.com>
2021-11-02 00:05:17 +0800

    mine
    删除了公共的接口，完成了注册的接口

--- script/connector-all.js
+++ script/connector-all.js
@@ -1,79 +1,100 @@
-//  获取按钮元素
-var btn = $(".Enter")
-var registerBtn = document.getElementsByClassName("skip");
-var emailVal = document.getElementsByClassName("Email");
-var warnE = document.getElementsByClassName("warn_E");
-var user = document.getElementsByClassName("User");
-var pwd = document.getElementsByClassName("Pass");
-var code = document.getElementsByClassName("Code");
+//  获取元素
+var $btn = $(".overall .Enter");
+var $warnE = $(".overall .warn_E");
+var $warn = $(".overall .warn");
+var $userVal = $('#User').val();
+var $pwdVal = $('#Pass').val();
+var $codeVal = $('#Code').val();
 
 
+/* 
+ * 当用户名失去焦点是 判断用户名是否可用 
+ */
 
-// 当用户名失去焦点是 判断用户名是否可用
-$(user).on("blur", function () {
-    $.post("http://1.14.68.137:8000/verify_username/", {
-        username: $(user).val()
-    },
-        function (data) {
-            console.log(data);
-        } 
-    )
+$("#User").on("blur", function () {
+    var str = $('#User').val();
+    var ret = /^[A-z][0-9A-z]{6,8}$/;
+    if (str) {
+        if (!str.match(ret)) {
+            $(".warn").text("用户名格式不正确")
+                $(".warn").css({
+                    display: "block"
+                })
+        } else {
+            $.post("http://1.14.68.137:8000/verify_username/", {
+                username: $("#User").val()
+            },
+                function (data) {
+                    switch (data.code) {
+                        case 0:
+                            $warn.text(data.msg);
+                            $warn.css("display", "block");
+                            break;
+                        case -1: 
+                            $warn.text(data.errmsg);
+                            $warn.css("display", "block");
+                            break;
+                    }
+                } 
+            )
+        }
+    }
+    setTimeout(function () {
+        $warn.css("display", "none");
+    }, 3000)
 })
 
 //  点击了发送按钮之后设置一个定时器
 var serialNumber = ""; 
-btn.on("click", function () {
+$btn.on("click", function () {
     $(this).attr("disabled", "disabled");
     var timer = 60;
     var timekeep = setInterval( function () {
         if (timer == 0) {
             clearInterval(timekeep);
-            $(btn).text("发送验证码");
+            $btn.text("发送验证码");
             timer = 60;
         } else {
             timer--;
-            $(btn).text("还剩下" + timer + "秒");
+            $btn.text("还剩下" + timer + "秒");
         }
     }, 1000)
 
     //  点击了发送验证码按钮之后，发送邮箱验证码请求
     $.post("http://1.14.68.137:8000/email/", {
-        email: $(emailVal).val()
+        email: $("#Email").val()
     },
         function (data) {
             console.log(data);
             if (data.code == 0) {
                 serialNumber = data.no;
-                $(warnE).css("display", "block");
-                $(warnE).text(data.msg);
-                disappear();
+                $warnE.css("display", "block");
+                $warnE.text(data.msg);
             } else {
-                $(warnE).css("display", "block");
-                $(warnE).text(data.errmsg);
-                disappear();
+                $warnE.css("display", "block");
+                $warnE.text(data.errmsg);
             }
+            setTimeout(function () {
+                $warnE.css("display", "none");
+            }, 3000)
         }
     )
 })
 
 
 
-function disappear() {
-    setTimeout(function () {
-        $(warnE).css("display", "none");
-    }, 3000)
-}
+    
 
 // 注册事件
-$(registerBtn).on("click", function () {
-    $.post("http://1.14.68.137:8000/register/", {
-        username: $(user).val(),
-        password: $(pwd).val(),
-        no: serialNumber,
-        text: $(code).val()
-    },
-        function (data) {
-            console.log(data);
-        }
-    )
-})
+// $(registerBtn).on("click", function () {
+//     $.post("http://1.14.68.137:8000/register/", {
+//         username: $(user).val(),
+//         password: $(pwd).val(),
+//         no: serialNumber,
+//         text: $(code).val()
+//     },
+//         function (data) {
+//             console.log(data);
+//         }
+//     )
+// })
--- script/register.js
+++ script/register.js
@@ -1,30 +1,52 @@
 $(function(){
-    // 用户名获取
-    $('.User').blur(function () {
-        var str = $('.User').val();
-        var ret = /^[A-z][0-9]{6,8}$/;
-       if (str){
-            if (!ret.test(str)){
-             $(".warn").css({
+    //  获取元素
+    var $btn = $(".overall .Enter");
+    var $warnE = $(".overall .warn_E");
+    var $warn = $(".overall .warn");
+    var $user = $('#User');
+    var $pwd = $('#Pass');
+    var $code = $('#Code');
+
+/* 
+ * 当用户名失去焦点是 判断用户名是否可用 
+ */
+
+$("#User").on("blur", function () {
+    var str = $user.val();
+    var ret = /^[A-z][0-9A-z]{6,8}$/;
+    if (str) {
+        if (!str.match(ret)) {
+            $(".warn").text("用户名格式不正确")
+                $(".warn").css({
                     display: "block"
                 })
-            }else{
-                $(".warn").css({
-                    display: "none"
-                })
-            }
-       }
-    })
-
-    $('.User').focus(function () {
-        $(".warn").css({
-            display: "none"
-        })
-    })
+        } else {
+            $.post("http://1.14.68.137:8000/verify_username/", {
+                username: $user.val()
+            },
+                function (data) {
+                    switch (data.code) {
+                        case 0:
+                            $warn.text(data.msg);
+                            $warn.css("display", "block");
+                            break;
+                        case -1: 
+                            $warn.text(data.errmsg);
+                            $warn.css("display", "block");
+                            break;
+                    }
+                } 
+            )
+        }
+    }
+    setTimeout(function () {
+        $warn.css("display", "none");
+    }, 3000)
+})
 
     // 密码获取
-    $('.Pass').blur(function () {
-        var str = $('.Pass').val();
+    $('#Pass').blur(function () {
+        var str = $pwd.val();
         var ret = /^[A-z0-9]{6,8}$/;
         if (str){
             if (!ret.test(str)){
@@ -39,15 +61,15 @@ $(function(){
        }
     })
 
-    $('.Pass').focus(function () {
+    $('#Pass').focus(function () {
         $(".warn_P").css({
             display: "none"
         })
     })
 
     // 确认密码获取
-    $('.Affirm').blur(function () {
-        if($('.Affirm').val() == $('.Pass').val()){
+    $('#Affirm').blur(function () {
+        if($('#Affirm').val() == $pwd.val()){
              $(".warn_A").css({
                 display: "none"
             })
@@ -58,15 +80,15 @@ $(function(){
         }
     })
 
-    $('.Affirm').focus(function () {
+    $('#Affirm').focus(function () {
         $(".warn_A").css({
             display: "none"
         })
     })
 
     // 电子邮箱获取
-    $('.Email').blur(function () {
-        var str = $('.Email').val();
+    $('#Email').blur(function () {
+        var str = $('#Email').val();
         var ret = /^[0-9A-z_]{5,16}(@qq)(\.com)$/;
         if (str){
             if (!ret.test(str)){
@@ -81,30 +103,71 @@ $(function(){
        }
     })
 
-    $('.Email').focus(function () {
+    $('#Email').focus(function () {
         $(".warn_E").css({
             display: "none"
         })
     })
 
-    // 验证码获取
-    // $('.Code').focus(function () {
-    //     $('.paste_C').css({
-    //         display : "none"
-    //     })
-    //     var thisVal = $(this).val();
-    //     if (thisVal != ""){
-    //         $(this).siblings("paste_C").hide();
-    //     }
-    // })
 
-    // $('.Code').blur(function () {
-    //     var thisVal = $(this).val(); 
-    //     if (thisVal != ""){
-    //         $(this).siblings($(".paste_C")).hide();
-    //     }else{
-    //         $(this).siblings($(".paste_C")).show();
-    //     }
-    // }) 
+    //  点击了发送按钮之后设置一个定时器
+    var serialNumber = ""; 
+    $btn.on("click", function () {
+        $(this).attr("disabled", "disabled");
+        var timer = 60;
+        var timekeep = setInterval( function () {
+            if (timer == 0) {
+                clearInterval(timekeep);
+                $btn.text("发送验证码");
+                timer = 60;
+            } else {
+                timer--;
+                $btn.text("还剩下" + timer + "秒");
+            }
+        }, 1000)
+
+    //  点击了发送验证码按钮之后，发送邮箱验证码请求
+        $.post("http://1.14.68.137:8000/email/", {
+            email: $("#Email").val()
+        },
+            function (data) {
+                console.log(data);
+                if (data.code == 0) {
+                    serialNumber = data.no;
+                    $warnE.css("display", "block");
+                    $warnE.text(data.msg);
+                } else {
+                    $warnE.css("display", "block");
+                    $warnE.text(data.errmsg);
+                }
+                setTimeout(function () {
+                    $warnE.css("display", "none");
+                }, 3000)
+            }
+        )
+    })
+
+
+
     
+
+    // 注册事件
+    $(".record .skip").on("click", function () {
+        $.post("http://1.14.68.137:8000/register/", {
+            username: $user.val(),
+            password: $pwd.val(),
+            no: serialNumber,
+            text: $code.val()
+        },
+            function (data) {
+               if (data.code == 0) {
+                   alert("注册成功，3秒后跳转登录页面");
+                   setInterval(function () {
+                       location.href = "http://127.0.0.1:5500/page/record.html";
+                   })
+               }
+            }
+        )
+    })
+
 })
